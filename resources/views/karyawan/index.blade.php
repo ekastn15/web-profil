438cfffe0786ad45cb98013e98d2d677fcac1855
ekastn15 <ekastn1506@gmail.com>
2024-10-25 10:09:48 +0700

    perbaruhi

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -4,7 +4,7 @@
  
 @section('contents')
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Karyawan</h1>
+<h1 class="h3 mb-2 text-gray-800">Pegawai</h1>
 
 @if(session('message'))
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
@@ -17,7 +17,7 @@
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <a href="{{route ('karyawan.insert')}}" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Add</a>
+                            <a href="{{route ('karyawan.insert')}}" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Tambah</a>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -38,7 +38,7 @@
             data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{ route('dinas') }}">Dinas</a>
-                <a class="collapse-item" href="{{ route('karyawan') }}">Karyawan</a>
+                <a class="collapse-item" href="{{ route('karyawan') }}">Pegawai</a>
             </div>
         </div>
     </li>
